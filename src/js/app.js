2fd97a321d3f9de1913bc4526e0e6fcbb2d95a09
Keenan <kkan68@gmail.com>
2021-12-02 21:03:27 +0000

    Progress v 0.2

--- src/js/app.js
+++ src/js/app.js
@@ -29,13 +29,24 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let name = variables.name;
+  if (variables.name == null) name = "Lucy";
+
+  let lastname = variables.lastname;
+  if (variables.lastname == null) lastname = "Boilett";
+
+  let role = variables.role;
+  if (variables.role == null) role = "Web Developer";
+
+  let city = variables.city;
+  if (variables.city == null) city = "Miami";
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${name} ${lastname}</h1>
+          <h2>${role}</h2>
+          <h3>${city}, USA</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
